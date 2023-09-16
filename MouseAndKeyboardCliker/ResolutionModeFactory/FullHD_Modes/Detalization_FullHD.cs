0d8f11876c7a5512d23ba9b1f3be0835786840f1
DDoS <ermakov123321123321@mail.ru>
2023-09-16 14:31:12 +0300

    Заполнил координаты для FullHD

--- MouseAndKeyboardCliker/ResolutionModeFactory/FullHD_Modes/Detalization_FullHD.cs
+++ MouseAndKeyboardCliker/ResolutionModeFactory/FullHD_Modes/Detalization_FullHD.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MouseAndKeyboardCliker
 {
-    public class Detalization_QuadHD : ModeBase
+    public class Detalization_FullHD : ModeBase
     {
         public override ElementCoords ExcelCell => _excelCell;
 
@@ -30,25 +30,25 @@ namespace MouseAndKeyboardCliker
         private ElementCoords _price;
         private ElementCoords _transferPrice;
 
-        public Detalization_QuadHD()
+        public Detalization_FullHD()
         {
-            _excelCell = new ElementCoords(1340, 325);
+            _excelCell = new ElementCoords(1020, 293);
 
-            _excelTextField = new ElementCoords(1540, 190, 1575, 230);
+            _excelTextField = new ElementCoords(1220, 180, 1250, 215);
 
-            _addButton = new ElementCoords(200, 705);
+            _addButton = new ElementCoords(150, 555);
 
-            _nomenclature = new ElementCoords(250, 990);
-            _nomenclature.ContextMenuInsert = new Coords(_nomenclature.Field.X + 50, _nomenclature.Field.Y -20);
-            _nomenclature.List = new Coords(300, 920);
+            _nomenclature = new ElementCoords(200, 770);
+            _nomenclature.ContextMenuInsert = new Coords(_nomenclature.Field.X + 40, _nomenclature.Field.Y - 15);
+            _nomenclature.List = new Coords(240, 720);
 
-            _amount = new ElementCoords(750, _nomenclature.Field.Y);
-            _amount.ContextMenuInsert = new Coords(800, _nomenclature.ContextMenuInsert.Y);
+            _amount = new ElementCoords(570, _nomenclature.Field.Y);
+            _amount.ContextMenuInsert = new Coords(610, _nomenclature.ContextMenuInsert.Y);
 
-            _price = new ElementCoords(900, _nomenclature.Field.Y);
-            _price.ContextMenuInsert = new Coords(950, _nomenclature.ContextMenuInsert.Y);
+            _price = new ElementCoords(690, _nomenclature.Field.Y);
+            _price.ContextMenuInsert = new Coords(730, _nomenclature.ContextMenuInsert.Y);
 
-            _transferPrice = new ElementCoords(1100, _nomenclature.Field.Y);
+            _transferPrice = new ElementCoords(850, _nomenclature.Field.Y);
         }
 
     }
